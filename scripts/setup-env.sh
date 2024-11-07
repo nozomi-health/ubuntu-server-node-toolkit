6dad57da551912084640373e15c46574e394b017
Alexander Kiseliov <kiselev20060@gmail.com>
2024-11-07 17:22:56 +0300

    fix: remove adding docker group

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -60,7 +60,6 @@ then
 
   sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-  sudo groupadd docker
   sudo usermod -aG docker ${USER}
   newgrp docker
 
